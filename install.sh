fd32d250c023e87a5ea5455bbd0ac28eea489922
Erick Christian <erickchristian48@gmail.com>
2024-09-21 10:16:14 +0700

    feat: more config

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo pacman -S alacritty fastfetch hyprland swaync waybar swww pipewire wireplumber qt5-wayland qt6-wayland xdg-desktop-portal-hyprland
+yay -S cava tofi
+
+cp -r ./hyprland-catppuccin/.config/* ~/.config
