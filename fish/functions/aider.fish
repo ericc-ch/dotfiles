e1311e409f514ee52d81b56d1e29733ddf727479
Erick Christian <erickchristian48@gmail.com>
2025-02-08 06:56:44 +0700

    aider why only yml and not yaml

--- fish/functions/aider.fish
+++ fish/functions/aider.fish
@@ -1,3 +1,4 @@
 function aider --description 'alias aider aider --config ~/.config/aider/.aider.conf.yaml'
-    command aider --config ~/.config/aider/.aider.conf.yaml $argv
+    command aider --config ~/.config/aider/.aider.conf.yml $argv
+
 end
