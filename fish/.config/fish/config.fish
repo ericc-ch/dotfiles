3cabdd887d4d97650076088dffc9f71196c74630
Erick Christian <erickchristian48@gmail.com>
2025-02-19 17:55:42 +0700

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,11 @@
 set -g fish_greeting
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# put random binaries here
+set --export PATH $HOME/.local/bin $PATH
+
+# go
+set --export PATH /usr/local/go/bin $PATH
