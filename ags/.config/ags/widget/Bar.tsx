aa0af17789504b7279af1ff9cd669ed65ebd5f6f
Erick Christian <erickchristian48@gmail.com>
2025-03-15 15:04:21 +0700

    feat: Display hyprland workspace id in bar

--- ags/.config/ags/widget/Bar.tsx
+++ ags/.config/ags/widget/Bar.tsx
@@ -1,15 +1,25 @@
 import { Variable } from "astal"
 import { App, Astal, Gtk, Gdk } from "astal/gtk4"
+import Hyprland from "gi://AstalHyprland"
 
 const time = Variable("").poll(1000, "date")
 
+const hyprland = Hyprland.get_default()
+
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-  const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor
+  const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
+  const clients = hyprland.get_clients()
+
+  for (const client of clients) {
+    console.log(client.workspace.get_id())
+  }
+
+  console.log(hyprland.get_focused_workspace().id)
 
   return (
     <window
       visible
-      anchor={TOP | RIGHT | BOTTOM}
+      anchor={TOP | RIGHT | LEFT}
       application={App}
       cssClasses={["Bar"]}
       exclusivity={Astal.Exclusivity.EXCLUSIVE}
