1adf9aae89ef323a44418014678ab775ab83f4b0
Erick Christian <erickchristian48@gmail.com>
2025-03-17 21:01:57 +0700

    refactor: Refactor date-time widget to use box and remove overlay

--- ags/.config/ags/widget/bar/date-time.tsx
+++ ags/.config/ags/widget/bar/date-time.tsx
@@ -5,28 +5,18 @@ import { getDay, isWeekend } from "../../lib/date"
 
 export function DateTime() {
   const time = Variable(new Date()).poll(1000, () => new Date())
-  const classes = Variable.derive([time], (time) => [
+
+  const dayClasses = Variable.derive([time], (time) => [
     "day",
     isWeekend(time) ? "weekend" : "weekday",
   ])
-  const bgText = Variable.derive([time], (time) =>
-    isWeekend(time) ? "WEEKEND" : "WEEKDAY",
-  )
 
   return (
-    <overlay cssClasses={["date-time"]}>
+    <box cssClasses={["date-time"]} orientation={Gtk.Orientation.VERTICAL}>
       <label
-        cssClasses={["bg-text"]}
-        label={bgText()}
-        type="overlay clip"
-      ></label>
-
-      <box orientation={Gtk.Orientation.VERTICAL}>
-        <label
-          label={time((time) => `${time.getMonth() + 1}/${time.getDate()}`)}
-        />
-        <label cssClasses={classes()} label={time((time) => getDay(time))} />
-      </box>
-    </overlay>
+        label={time((time) => `${time.getMonth() + 1}/${time.getDate()}`)}
+      />
+      <label cssClasses={dayClasses()} label={time((time) => getDay(time))} />
+    </box>
   )
 }
