64582c7fb5007d613cee367d99bc6915632a3eff
Erick Christian <erickchristian48@gmail.com>
2025-03-18 09:53:22 +0700

    fix: Add --noconfirm flag to pacman command in update-eos function

--- fish/.config/fish/functions/update-eos.fish
+++ fish/.config/fish/functions/update-eos.fish
@@ -1,6 +1,6 @@
 function update-eos --description 'alias update eos-update --yay'
   yes | eos-update --yay
-  sudo pacman -Qdtq | sudo pacman -Rns -
+  sudo pacman -Qdtq | sudo pacman -Rns --noconfirm -
 
   flatpak update -y
   flatpak uninstall --unused
