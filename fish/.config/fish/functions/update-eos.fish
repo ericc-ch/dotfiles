fe10665b482b4efa1723205a6a2891a62d0e6a2f
Erick Christian <erickchristian48@gmail.com>
2025-03-18 09:51:34 +0700

    fix: Use sudo for removing orphaned packages

--- fish/.config/fish/functions/update-eos.fish
+++ fish/.config/fish/functions/update-eos.fish
@@ -1,6 +1,6 @@
 function update-eos --description 'alias update eos-update --yay'
   yes | eos-update --yay
-  sudo pacman -Qdtq | pacman -Rns -
+  sudo pacman -Qdtq | sudo pacman -Rns -
 
   flatpak update -y
   flatpak uninstall --unused
