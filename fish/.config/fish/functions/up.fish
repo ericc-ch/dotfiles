03189feb41d5c69d70e9210d81ab1dc536d203ae
Erick Christian <erickchristian48@gmail.com>
2025-11-23 01:04:05 +0700

    noconfirm

--- fish/.config/fish/functions/up.fish
+++ fish/.config/fish/functions/up.fish
@@ -1,7 +1,7 @@
 function up --description 'update system'
-    paru -Syu
-    paru -c
+    paru -Syu --noconfirm
+    paru -c --noconfirm
 
     flatpak update -y
-    flatpak uninstall --unused
+    flatpak uninstall --unused -y
 end
