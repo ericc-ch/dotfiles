83ea5feed197b8b1859a844eac1a54998564d853
Erick Christian <erickchristian48@gmail.com>
2025-12-16 21:18:04 +0700

    remove bun option from up command

--- fish/.config/fish/functions/up.fish
+++ fish/.config/fish/functions/up.fish
@@ -1,24 +1,19 @@
 function up --description 'update system'
-    argparse 's/system' 'f/flatpak' 'b/bun' -- $argv
+    argparse 's/system' 'f/flatpak' -- $argv
     or return 1
 
-    # If no flags, run all
-    set -l run_all
-    if not set -q _flag_system; and not set -q _flag_flatpak; and not set -q _flag_bun
+    set -l run_all 0
+    if not set -q _flag_system; and not set -q _flag_flatpak
         set run_all 1
     end
 
-    if set -q run_all; or set -q _flag_system
+    if test $run_all -eq 1; or set -q _flag_system
         paru -Syu --noconfirm
         paru -c --noconfirm
     end
 
-    if set -q run_all; or set -q _flag_flatpak
+    if test $run_all -eq 1; or set -q _flag_flatpak
         flatpak update -y
         flatpak uninstall --unused -y
-    end
-
-    if set -q run_all; or set -q _flag_bun
-        bun update -g
     end
 end
