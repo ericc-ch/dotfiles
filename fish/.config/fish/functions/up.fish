693efc675d89c5451c52c9a7d59bd460ebc1e0b7
Erick Christian <erickchristian48@gmail.com>
2026-01-16 13:30:50 +0700

    simplify up

--- fish/.config/fish/functions/up.fish
+++ fish/.config/fish/functions/up.fish
@@ -1,19 +1,7 @@
 function up --description 'update system'
-    argparse 's/system' 'f/flatpak' -- $argv
-    or return 1
+  paru -Syu --noconfirm
+  paru -c --noconfirm
 
-    set -l run_all 0
-    if not set -q _flag_system; and not set -q _flag_flatpak
-        set run_all 1
-    end
-
-    if test $run_all -eq 1; or set -q _flag_system
-        paru -Syu --noconfirm
-        paru -c --noconfirm
-    end
-
-    if test $run_all -eq 1; or set -q _flag_flatpak
-        flatpak update -y
-        flatpak uninstall --unused -y
-    end
+  flatpak update -y
+  flatpak uninstall --unused -y
 end
